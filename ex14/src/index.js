a0da61668733296b415c78b80a2d0106889c1000
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-20 13:38:08 +0200

    Created folder 14

---
+++ ex14/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+
+console.log(convertStrToInt("27"));
+
+// Only change code above this line
+    
+module.exports = convertStrToInt;
