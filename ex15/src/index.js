74f10238fd8a7c7ed5cf55a73f7e7225647d7559
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-20 13:58:09 +0200

    Created folder 15

---
+++ ex15/src/index.js
@@ -0,0 +1,11 @@
+// Only change code below this line
+
+function convertToInt(str) {
+    return parseInt(str, 16);
+}
+
+console.log(convertToInt("BA"));
+
+// Only change code above this line
+    
+module.exports = convertToInt;
